cc45d427a1ef2ab235eb959efd36de1b82354f0b
Sergey Chernov <real.sergeych@gmail.com>
2017-09-19 03:35:38 +0300

    refs #2635 parallelism tests for extended signature

--- universa_core/src/test/java/com/icodici/universa/contract/ExtendedSignatureTest.java
+++ universa_core/src/test/java/com/icodici/universa/contract/ExtendedSignatureTest.java
@@ -14,9 +14,17 @@ import com.icodici.universa.node.network.TestKeys;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
 public class ExtendedSignatureTest extends TestCase{
 
@@ -31,5 +39,68 @@ public class ExtendedSignatureTest extends TestCase{
         assertAlmostSame(es.getCreatedAt(), LocalDateTime.now());
         assertEquals(ExtendedSignature.keyId(k), ExtendedSignature.keyId(pubKey));
         assertEquals(ExtendedSignature.keyId(k), ExtendedSignature.extractKeyId(signature));
+    }
+
+    @Test
+    public void parallelExecutionSign() throws Exception {
+        ExecutorService single = Executors.newSingleThreadExecutor();
+        ExecutorService multiple = Executors.newCachedThreadPool();
+
+        byte[] data = "Hello world".getBytes();
+        PrivateKey k = new PrivateKey(2048);
+
+        // warm up
+        for(int i=0; i<200; i++)
+            ExtendedSignature.sign(k, data);
+
+        double t1 = parallelize(single, 1, ()-> {
+            for(int i=0; i<100; i++)
+                ExtendedSignature.sign(k, data);
+        });
+        System.out.println(t1);
+        double t2 = parallelize(multiple, 4, ()-> {
+            for(int i=0; i<100; i++)
+                ExtendedSignature.sign(k, data);
+        });
+        System.out.println(t2);
+        assertThat(Math.abs(t1-t2), is(lessThan(0.15)));
+    }
+
+    @Test
+    public void parallelExecutionVerify() throws Exception {
+        ExecutorService single = Executors.newSingleThreadExecutor();
+        ExecutorService multiple = Executors.newCachedThreadPool();
+
+        byte[] data = "Hello world".getBytes();
+        PrivateKey k = new PrivateKey(2048);
+        PublicKey key = k.getPublicKey();
+        byte[] signature = ExtendedSignature.sign(k, data);
+
+        // warm up
+        for(int i=0; i<200; i++)
+            ExtendedSignature.sign(k, data);
+
+        double t1 = parallelize(single, 1, ()-> {
+            for(int i=0; i<1000; i++)
+                ExtendedSignature.verify(key, signature, data);
+        });
+        System.out.println(t1);
+        double t2 = parallelize(multiple, 4, ()-> {
+            for(int i=0; i<1000; i++)
+                ExtendedSignature.verify(key, signature, data);
+        });
+        System.out.println(t2);
+        assertThat(Math.abs(t1-t2), is(lessThan(0.15)));
+    }
+
+    double parallelize(ExecutorService es,int nThreads,Runnable r) throws ExecutionException, InterruptedException {
+        long t = System.nanoTime();
+        ArrayList<Future<?>> all = new ArrayList<>();
+        for( int i=0; i < nThreads; i++ ) {
+            all.add( es.submit(()->r.run()));
+        }
+        for(Future<?> f: all)
+            f.get();
+        return (System.nanoTime() - t) * 1e-9;
     }
 }
