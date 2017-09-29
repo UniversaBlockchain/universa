024114f83e8f724f21558e7673f7195313f5e567
pogorelov <Pogorelov1>
2017-09-29 11:24:00 +0300

    refs #2641 the ListRole is implemented, created ListRoleTest.

--- universa_core/src/main/java/com/icodici/universa/contract/roles/ListRole.java
+++ universa_core/src/main/java/com/icodici/universa/contract/roles/ListRole.java
@@ -2,18 +2,134 @@
  * Copyright (c) 2017 Sergey Chernov, iCodici S.n.C, All Rights Reserved
  *
  * Written by Sergey Chernov <real.sergeych@gmail.com>
+ * Written by Maxim Pogorelov <pogorelovm23@gmail.com>, 9/28/17.
  *
  */
 
 package com.icodici.universa.contract.roles;
 
+import com.icodici.crypto.AbstractKey;
+import com.icodici.crypto.PublicKey;
+import com.icodici.universa.contract.KeyRecord;
+import com.sun.istack.internal.NotNull;
+import net.sergeych.biserializer.BiDeserializer;
+import net.sergeych.biserializer.BiSerializer;
 import net.sergeych.biserializer.BiType;
+import net.sergeych.biserializer.DefaultBiMapper;
+import net.sergeych.tools.Binder;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Role combining other roles (sub-roles) in the "and", "or" and "any N of" principle.
  */
 @BiType(name = "ListRole")
-public abstract class ListRole extends Role {
+public class ListRole extends Role {
+
+    private Mode mode;
+
+    private Set<Role> roles = new HashSet<>();
+
+    private int quorumSize = 0;
+
+    public ListRole() {
+    }
+
+    public ListRole(String name) {
+        super(name);
+    }
+
+    public ListRole(String name, Mode mode, @NotNull Collection<Role> roles) {
+        super(name);
+        setMode(mode);
+        addAll(roles);
+    }
+
+    public ListRole(String name, int quorumSize, @NotNull Collection<Role> roles) {
+        super(name);
+        this.mode = Mode.QUORUM;
+        this.quorumSize = quorumSize;
+        addAll(roles);
+    }
+
+    public void addAll(Collection<Role> roles) {
+        this.roles.addAll(roles);
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public ListRole addRole(Role role) {
+        this.roles.add(role);
+        return this;
+    }
+
+    /**
+     * Set mode to {@link Mode#QUORUM} and quorum size to any n roles.
+     *
+     * @param n how many subroles set of key must be able to play to play this role
+     */
+    public void setQuorum(int n) {
+        mode = Mode.QUORUM;
+        quorumSize = n;
+    }
+
+    /**
+     * @return quorum size if in quorum mode, otherwise 0
+     */
+    public int getQuorum() {
+        return Mode.QUORUM.equals(mode) ? this.quorumSize : 0;
+    }
+
+    /**
+     * Set mode to either {@link Mode#ALL} or {@link Mode#ANY}, Quorum mode could be set only with {@link #setQuorum(int)}
+     * call,
+     *
+     * @param newMode mode to set
+     * @throws IllegalArgumentException if mode other than ANY/ALL is specified
+     */
+    public void setMode(Mode newMode) {
+        if (!Mode.QUORUM.equals(newMode))
+            this.mode = newMode;
+        else
+            throw new IllegalArgumentException("Only ANY or ALL of the modes should be set.");
+    }
+
+
+    @Override
+    public boolean isAllowedForKeys(Set<? extends AbstractKey> keys) {
+        return Mode.ANY.equals(this.mode) && this.processAnyMode(keys) ||
+                Mode.ALL.equals(this.mode) && this.processAllMode(keys) ||
+                Mode.QUORUM.equals(this.mode) && this.processQuorumMode(keys);
+    }
+
+    private boolean processQuorumMode(Set<? extends AbstractKey> keys) {
+        long matchNumber = this.roles.stream().filter(role -> role.isAllowedForKeys(keys)).count();
+        return matchNumber >= this.quorumSize;
+    }
+
+    private boolean processAllMode(Set<? extends AbstractKey> keys) {
+        return this.roles.stream().allMatch(role -> role.isAllowedForKeys(keys));
+    }
+
+    private boolean processAnyMode(Set<? extends AbstractKey> keys) {
+        return this.roles.stream().anyMatch(role -> role.isAllowedForKeys(keys));
+    }
+
+    @Override
+    public boolean isValid() {
+        return !this.roles.isEmpty();
+    }
+
+    @Override
+    public Set<PublicKey> getKeys() {
+        return this.roles.stream()
+                .flatMap(role -> role.getKeyRecords().stream()
+                        .map(KeyRecord::getPublicKey))
+                .collect(Collectors.toSet());
+    }
 
     /**
      * Mode of combining roles
@@ -22,39 +138,72 @@ public abstract class ListRole extends Role {
         /**
          * Role could be performed only if set of keys could play all sub-roles
          */
-        ALL,
+        ALL("all"),
         /**
          * Role could be performed if set of keys could play any role of the list
          */
-        ANY,
+        ANY("any"),
         /**
          * Role could be played if set of keys could play any quorrum set of roles, e.g. at least any N subroles,
          * controlled by the {@link #setQuorum(int)} method
          */
-        QUORUM
+        QUORUM("quorum");
+
+        private String value;
+
+        Mode(String v) {
+            this.value = v;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
     }
 
-    private int quorumSize = 0;
+    @Override
+    public String toString() {
+        return String.format("ListRole<%s:%s:%s:%s>", System.identityHashCode(this), getName(),
+                Mode.QUORUM.equals(this.mode) ? this.mode + "_" + this.quorumSize : this.mode, this.roles);
+    }
 
-    /**
-     * Set mode to {@link Mode#QUORUM} and quorum size to any n roles.
-     *
-     * @param n how many subroles set of key must be able to play to play this role
-     */
-    abstract void setQuorum(int n);
+    @Override
+    public void deserialize(Binder data, BiDeserializer deserializer) {
+        super.deserialize(data, deserializer);
 
-    /**
-     * @return quorum size if in quorum mode, otherwise 0
-     */
-    abstract int getQuorum();
+        Object quorumSize = data.getOrDefault("quorumSize", null);
+        if (quorumSize != null) {
+            try {
+                this.quorumSize = quorumSize instanceof Integer ? (Integer) quorumSize
+                        : quorumSize instanceof String ? Integer.parseInt((String) quorumSize)
+                        : 0;
+            } catch (NumberFormatException ignored) {
+            }
+        }
 
-    /**
-     * Set mode to either {@link Mode#ALL} or {@link Mode#ANY}, Quorum mode could be set only with {@link #setQuorum(int)}
-     * call,
-     *
-     * @param newMode mode to set
-     *
-     * @throws IllegalArgumentException if mode other than ANY/ALL is specified
-     */
-    abstract void setMode(Mode newMode);
+        Object mode = data.getOrDefault("mode", null);
+        if (mode != null && mode instanceof Mode) {
+            this.mode = (Mode) mode;
+        }
+
+        List<Binder> roles = data.getList("roles", null);
+        if (roles != null) {
+            this.roles.clear();
+            roles.forEach(role -> addRole(deserializer.deserialize(role)));
+        }
+
+
+    }
+
+    @Override
+    public Binder serialize(BiSerializer s) {
+        return super.serialize(s).putAll(
+                "quorumSize", s.serialize(this.quorumSize),
+                "mode", s.serialize(this.mode),
+                "roles", s.serialize(this.roles));
+    }
+
+    static {
+        DefaultBiMapper.registerClass(ListRole.class);
+    }
 }
---
+++ universa_core/src/test/java/com/icodici/universa/contract/roles/ListRoleTest.java
@@ -0,0 +1,258 @@
+/*
+ * Created by Maxim Pogorelov <pogorelovm23@gmail.com>, 9/28/17.
+ */
+
+package com.icodici.universa.contract.roles;
+
+import com.icodici.crypto.AbstractKey;
+import com.icodici.crypto.EncryptionError;
+import com.icodici.crypto.PrivateKey;
+import com.icodici.universa.contract.Contract;
+import com.icodici.universa.contract.KeyRecord;
+import com.icodici.universa.node.network.TestKeys;
+import net.sergeych.biserializer.DefaultBiMapper;
+import net.sergeych.tools.Binder;
+import net.sergeych.tools.Do;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+
+public class ListRoleTest {
+
+    static List<PrivateKey> keys = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 4; i++)
+            try {
+                keys.add(TestKeys.privateKey(i));
+            } catch (EncryptionError encryptionError) {
+                encryptionError.printStackTrace();
+            }
+    }
+
+    @Test
+    public void shouldPerformRoleWithAnyMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink s2 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(1).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(s2);
+
+        ListRole roleList = new ListRole("listAnyMode", ListRole.Mode.ANY, Do.listOf(s1, s2));
+
+        assertTrue(roleList.isAllowedForKeys(new HashSet<>(Do.listOf(keys.get(1).getPublicKey()))));
+    }
+
+    @Test
+    public void shouldNotPerformRoleWithAnyMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink s2 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(s2);
+
+        ListRole roleList = new ListRole("listAnyMode");
+        roleList.setMode(ListRole.Mode.ANY);
+        roleList.addRole(s1)
+                .addRole(s2);
+
+
+        assertFalse(roleList.isAllowedForKeys(new HashSet<>(Do.listOf(keys.get(1).getPublicKey()))));
+    }
+
+    @Test
+    public void shouldPerformRoleWithAllMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink s2 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+        s1.addKeyRecord(new KeyRecord(keys.get(1).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(s2);
+
+        ListRole roleList = new ListRole("listAllMode");
+        roleList.setMode(ListRole.Mode.ALL);
+        roleList.addRole(s1)
+                .addRole(s2);
+
+
+        HashSet<AbstractKey> keys = new HashSet<>(Do.listOf(
+                ListRoleTest.keys.get(0).getPublicKey(),
+                ListRoleTest.keys.get(1).getPublicKey()));
+
+        assertTrue(roleList.isAllowedForKeys(keys));
+    }
+
+    @Test
+    public void shouldNotPerformRoleWithAllMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink l1 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+
+        SimpleRole s2 = new SimpleRole("owner2");
+        s2.addKeyRecord(new KeyRecord(keys.get(1).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(l1);
+        c.registerRole(s2);
+
+        ListRole roleList = new ListRole("listAllMode", ListRole.Mode.ALL, Do.listOf(s1, s2));
+
+        HashSet<AbstractKey> keys = new HashSet<>(Do.listOf(
+                ListRoleTest.keys.get(1).getPublicKey()));
+
+        assertFalse(roleList.isAllowedForKeys(keys));
+    }
+
+    @Test
+    public void shouldNotAllowToSetQuorum() {
+        ListRole listRole = new ListRole("roles");
+
+        try {
+            listRole.setMode(ListRole.Mode.QUORUM);
+
+            fail("Expected exception to be thrown.");
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().equalsIgnoreCase("Only ANY or ALL of the modes should be set."));
+        }
+    }
+
+    @Test
+    public void shouldNotAllowToSetQuorumInConstructor() {
+        try {
+            new ListRole("roles", ListRole.Mode.QUORUM, Collections.emptyList());
+
+            fail("Expected exception to be thrown.");
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().equalsIgnoreCase("Only ANY or ALL of the modes should be set."));
+        }
+    }
+
+    @Test
+    public void shouldPerformRoleWithQuorumMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink l1 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+
+        SimpleRole s2 = new SimpleRole("owner2");
+        s2.addKeyRecord(new KeyRecord(keys.get(1).getPublicKey()));
+
+        SimpleRole s3 = new SimpleRole("owner3");
+        s3.addKeyRecord(new KeyRecord(keys.get(3).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(l1);
+        c.registerRole(s2);
+        c.registerRole(s3);
+
+        ListRole roleList = new ListRole("listQuorumMode", 2, Do.listOf(s1, s2, s3, l1));
+
+
+        HashSet<AbstractKey> keys = new HashSet<>(Do.listOf(
+                ListRoleTest.keys.get(0).getPublicKey(),
+                ListRoleTest.keys.get(2).getPublicKey()));
+
+        assertTrue(roleList.isAllowedForKeys(keys));
+    }
+
+    @Test
+    public void shouldNotPerformRoleWithQuorumMode() {
+        Contract c = new Contract();
+
+        SimpleRole s1 = new SimpleRole("owner");
+        RoleLink l1 = new RoleLink("master", "owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+
+        SimpleRole s2 = new SimpleRole("owner2");
+        s2.addKeyRecord(new KeyRecord(keys.get(1).getPublicKey()));
+
+        c.registerRole(s1);
+        c.registerRole(l1);
+        c.registerRole(s2);
+
+        ListRole roleList = new ListRole("listQuorumMode");
+        roleList.setQuorum(2);
+        roleList.addAll(Do.listOf(s1, s2, l1));
+
+
+        HashSet<AbstractKey> keys = new HashSet<>(Do.listOf(
+                ListRoleTest.keys.get(1).getPublicKey()));
+
+        assertFalse(roleList.isAllowedForKeys(keys));
+    }
+
+    @Test
+    public void serializeAll() throws Exception {
+        SimpleRole s1 = new SimpleRole("owner");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+
+        ListRole roleList = new ListRole("listAllMode", ListRole.Mode.ALL, Do.listOf(s1));
+
+        Binder serialized = DefaultBiMapper.serialize(roleList);
+        Role r1 = DefaultBiMapper.deserialize(serialized);
+        assertEquals(r1, roleList);
+    }
+
+    @Test
+    public void serializeAny() throws Exception {
+        SimpleRole s1 = new SimpleRole("owner");
+        SimpleRole s2 = new SimpleRole("owner2");
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+        s2.addKeyRecord(new KeyRecord(keys.get(2).getPublicKey()));
+
+        ListRole roleList = new ListRole("listAnyMode", ListRole.Mode.ANY, Do.listOf(s1, s2));
+
+        Binder serialized = DefaultBiMapper.serialize(roleList);
+        Role r1 = DefaultBiMapper.deserialize(serialized);
+        assertEquals(r1, roleList);
+    }
+
+
+    @Test
+    public void serializeQuorum() throws Exception {
+        SimpleRole s1 = new SimpleRole("owner");
+        SimpleRole s2 = new SimpleRole("owner2");
+        RoleLink l1 = new RoleLink("link1", "owner");
+
+
+        s1.addKeyRecord(new KeyRecord(keys.get(0).getPublicKey()));
+        s2.addKeyRecord(new KeyRecord(keys.get(2).getPublicKey()));
+
+        ListRole roleList = new ListRole("listAnyMode", 2, Do.listOf(s1, s2, l1));
+
+        Binder serialized = DefaultBiMapper.serialize(roleList);
+        Role r1 = DefaultBiMapper.deserialize(serialized);
+        assertEquals(r1, roleList);
+    }
+
+
+}
