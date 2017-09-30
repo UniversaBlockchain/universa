4658772d7d3ac5f61865d78512da93f3626833b5
Sergey Chernov <real.sergeych@gmail.com>
2017-09-30 13:20:22 +0300

    refs #2665 fixes #2706 decimal class to work with univeresa currency- type values

---
+++ universa_core/src/test/java/com/icodici/universa/DecimalTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Sergey Chernov, iCodici S.n.C, All Rights Reserved
+ *
+ * Written by Sergey Chernov <real.sergeych@gmail.com>
+ *
+ */
+
+package com.icodici.universa;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DecimalTest {
+
+    @Test
+    public void divideAndRemainder() throws Exception {
+        Decimal x = new Decimal("1000000000000");
+        Decimal[] dr = x.divideAndRemainder(new Decimal(3));
+        assertEquals("333333333333", dr[0].toString());
+        assertEquals(1, dr[1].intValue());
+    }
+
+    @Test
+    public void testPrecision() throws Exception {
+        Decimal x = new Decimal("1000000000000");
+        Decimal y = x.divide(new Decimal(3));
+        assertEquals("0.333333333", y.getFraction().toString());
+        assertEquals("333333333333.333333333", y.toString());
+    }
+
+    @Test
+    public void ulp() throws Exception {
+        Decimal x = new Decimal("1000000000000.0000000001");
+        assertEquals( 1e-10,x.ulp().doubleValue(), 0);
+    }
+
+}
