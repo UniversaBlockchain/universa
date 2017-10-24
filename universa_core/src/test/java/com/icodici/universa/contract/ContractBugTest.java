be886bb361b689ff5ec3572e1a9975e2225c794f
stipan <micromillioner@yahoo.com>
2017-10-24 07:48:38 -0700

    Added ContractBugTest file to test Contract creation from sealed bug.
    Notice: uncomment loadContractHook() function to avoid bug.

---
+++ universa_core/src/test/java/com/icodici/universa/contract/ContractBugTest.java
@@ -0,0 +1,43 @@
+package com.icodici.universa.contract;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class ContractBugTest extends ContractTestBase {
+
+
+    @Test
+    public void createFromSealed() throws Exception {
+        // Contract bug: Contract cannot be initiated from sealed data until
+        // Permissions being created or initialized or something like that. To avoid it now uncomment string below.
+//        loadContractHook();
+
+        Contract contract = null;
+
+        String fileName = "./src/test_contracts/simple_root_contract.unc";
+
+        Path path = Paths.get(fileName);
+        byte[] data = Files.readAllBytes(path);
+
+        try {
+            contract = new Contract(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertNotEquals(contract, null);
+    }
+
+    // This method is a hook, it resolve Contract bug: Contract cannot be initiated from sealed data until
+    // Permissions beaing created or initialized or something like that.
+    private void loadContractHook() throws IOException {
+        Contract.fromYamlFile("./src/test_contracts/simple_root_contract.yml");
+    }
+}
