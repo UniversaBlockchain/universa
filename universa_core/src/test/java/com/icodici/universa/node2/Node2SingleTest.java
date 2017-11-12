8da2a7552132c6167c8b2c365679daa7e2079c50
Maxim Pogorelov <pogorelovm23@gmail.com>
2017-11-12 16:43:57 +0200

    Caught a concurrent modification exception. Test called 'unexpectedStrangeCaseWithConcurrent' in Node2EmulatedNetworkTest.

--- universa_core/src/test/java/com/icodici/universa/node2/Node2SingleTest.java
+++ universa_core/src/test/java/com/icodici/universa/node2/Node2SingleTest.java
@@ -451,45 +451,6 @@ public class Node2SingleTest extends TestCase {
     }
 
     @Test
-    public void checkSergeychCase() throws Exception {
-        String transactionName = "./src/test_contracts/transaction/b8f8a512-8c45-4744-be4e-d6788729b2a7.transaction";
-
-        for( int i=0; i < 5; i++) {
-            Contract contract = readContract(transactionName, true);
-
-            HashId id;
-            StateRecord orCreate;
-
-            for (Approvable c : contract.getRevokingItems()) {
-                id = c.getId();
-                orCreate = ledger.findOrCreate(id);
-                orCreate.setState(ItemState.APPROVED).save();
-            }
-
-            for( Approvable c: contract.getNewItems()) {
-                orCreate = ledger.getRecord(c.getId());
-                if( orCreate != null )
-                    orCreate.destroy();
-            }
-
-            StateRecord r = ledger.getRecord(contract.getId());
-            if( r !=  null ) {
-                r.destroy();
-            }
-
-            contract.check();
-            contract.traceErrors();
-            assertTrue(contract.isOk());
-
-            node.registerItem(contract);
-            ItemResult itemResult = node.waitItem(contract.getId(), 15000);
-            if( ItemState.APPROVED != itemResult.state)
-                fail("Wrong state on repetition "+i+": "+itemResult+", "+itemResult.errors);
-            assertEquals(ItemState.APPROVED, itemResult.state);
-        }
-    }
-
-    @Test
     public void shouldApproveSplit() throws Exception {
         // 100
         Contract c = Contract.fromDslFile(ROOT_PATH + "coin100.yml");
