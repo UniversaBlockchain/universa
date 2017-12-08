25c96c98157beff273054cc01b4f8846c7d27acc
stipan <micromillioner@yahoo.com>
2017-12-08 03:41:26 -0800

    Added blank test class for common network tests

---
+++ universa_core/src/test/java/com/icodici/universa/node2/BaseNetworkTest.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2017, All Rights Reserved
+ *
+ * Written by Stepan Mamontov <micromillioner@yahoo.com>
+ */
+
+package com.icodici.universa.node2;
+
+import com.icodici.universa.node.TestCase;
+
+public class BaseNetworkTest extends TestCase {
+}
