ad6383070f0b1da370e629698e2c435579c22659
stipan <micromillioner@yahoo.com>
2017-12-22 03:05:47 -0800

    added references to all steps of swap procedure

--- universa_core/src/main/java/com/icodici/universa/contract/ReferenceRole.java
+++ universa_core/src/main/java/com/icodici/universa/contract/ReferenceRole.java
@@ -1,11 +1,40 @@
 package com.icodici.universa.contract;
 
-public class ReferenceRole {
+import com.icodici.universa.HashId;
+import net.sergeych.biserializer.BiDeserializer;
+import net.sergeych.biserializer.BiSerializable;
+import net.sergeych.biserializer.BiSerializer;
+import net.sergeych.biserializer.DefaultBiMapper;
+import net.sergeych.tools.Binder;
+import net.sergeych.utils.Bytes;
+
+import java.util.ArrayList;
+
+public class ReferenceRole implements BiSerializable {
     public String name;
     public byte[] fingerprint;
 
     public ReferenceRole(String name, byte[] fingerprint) {
         this.name = name;
         this.fingerprint = fingerprint;
+    }
+
+    @Override
+    public void deserialize(Binder data, BiDeserializer deserializer) {
+        this.name = data.getString("name", null);
+
+        this.fingerprint = data.getBinary("type");
+    }
+
+    @Override
+    public Binder serialize(BiSerializer s) {
+        Binder data = new Binder();
+        data.set("name", s.serialize(this.name));
+        data.set("fingerprint", s.serialize(this.fingerprint));
+        return data;
+    }
+
+    static {
+        DefaultBiMapper.registerClass(ReferenceRole.class);
     }
 }
