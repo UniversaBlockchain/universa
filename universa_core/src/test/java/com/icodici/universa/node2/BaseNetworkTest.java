053f032fe085afdaf878aa0d4ee666b9a491e8ca
Leonid Novikov <nil182@mail.ru>
2017-12-24 18:19:20 +0300

    make calls of checkReferencedItems recursively

--- universa_core/src/test/java/com/icodici/universa/node2/BaseNetworkTest.java
+++ universa_core/src/test/java/com/icodici/universa/node2/BaseNetworkTest.java
@@ -695,6 +695,7 @@ public class BaseNetworkTest extends TestCase {
     private void registerAndCheckApproved(Contract c) throws TimeoutException, InterruptedException {
         node.registerItem(c);
         ItemResult itemResult = node.waitItem(c.getId(), 8000);
+        c.traceErrors();
         assertEquals(ItemState.APPROVED, itemResult.state);
     }
 
