5ea3d972fcc225b5bd9dc7e16f722d39aaf34564
stipan <micromillioner@yahoo.com>
2017-12-26 03:15:41 -0800

    Cleaned up ContractsService

--- universa_core/src/main/java/com/icodici/universa/contract/ContractsService.java
+++ universa_core/src/main/java/com/icodici/universa/contract/ContractsService.java
@@ -20,68 +20,14 @@ import java.util.*;
 
 public class ContractsService {
 
-//    public ContractsService(byte[] sealed) throws IOException {
-//        super(sealed);
-//    }
-//
-//    public ContractsService(byte[] sealed, TransactionPack pack) throws IOException {
-//        super(sealed, pack);
-//    }
-//
-//    public ContractsService() {
-//        super();
-//        Definition cd = getDefinition();
-//        // by default, transactions expire in 30 days
-//        cd.setExpiresAt(getCreatedAt().plusDays(30));
-//    }
-//
-//    /**
-//     * The only call needed to setup roles and rights and signatures for {@link ContractsService}.
-//     * <p>
-//     * Transaction contract is immutable, and is issued, onwed and created by the same role, so we create it all in one
-//     * place, with at least one privateKey. Do not change any roles directly.
-//     *
-//     * @param issuers
-//     */
-//    public void setIssuer(PrivateKey... issuers) {
-//        SimpleRole issuerRole = new SimpleRole("issuer");
-//        for (PrivateKey k : issuers) {
-//            KeyRecord kr = new KeyRecord(k.getPublicKey());
-//            issuerRole.addKeyRecord(kr);
-//            addSignerKey(k);
-//        }
-//        registerRole(issuerRole);
-//        createRole("owner", issuerRole);
-//        createRole("creator", issuerRole);
-//    }
-//
-//    public void setIssuer(PublicKey... swapperKeys) {
-//        ListRole issuerRole = new ListRole("issuer");
-//        for (int i = 0; i < swapperKeys.length; i++) {
-//            SimpleRole swapperRole = new SimpleRole("swapper" + (i+1));
-//
-//            swapperRole.addKeyRecord(new KeyRecord(swapperKeys[i]));
-//
-//            registerRole(swapperRole);
-//
-//            issuerRole.addRole(swapperRole);
-//        }
-////        issuerRole.setMode(ListRole.Mode.ALL);
-//
-//        registerRole(issuerRole);
-//        createRole("owner", issuerRole);
-//        createRole("creator", issuerRole);
-//    }
-//
-//    public void addContractToRemove(Contract c) {
-//        if( !getRevokingItems().contains(c)) {
-//            Binder data = getDefinition().getData();
-//            List<Binder> actions = data.getOrCreateList("actions");
-//            getRevokingItems().add(c);
-//            actions.add(Binder.fromKeysValues("action", "remove", "id", c.getId()));
-//        }
-//    }
-
+    /**
+     * Implementing revoking procedure.
+     * Service create temp contract with given contract in revoking items and return it.
+     * That temp contract should be send to Universa and given contract will be revoked.
+     * @param c - contract should revoked be
+     * @param keys - keys from owner of c
+     * @return
+     */
     public static Contract createRevocation(Contract c, PrivateKey... keys) {
 
         Contract tc = new Contract();
