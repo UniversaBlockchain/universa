ae62f13038559acb6c2a23c0b33ef30dea34a898
kalinkin <kalinkineo@gmail.com>
2018-02-02 17:15:48 +0300

    Serialization/deserialization test parcel

---
+++ universa_core/src/test/java/com/icodici/universa/contract/ParcelTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2018 Sergey Chernov, iCodici S.n.C, All Rights Reserved
+ *
+ * Written by WildCats <kalinkineo@gmail.com>, February 2018.
+ *
+ */
+
+package com.icodici.universa.contract;
+
+import com.icodici.universa.contract.roles.RoleLink;
+import com.icodici.universa.node.network.TestKeys;
+import net.sergeych.biserializer.BiDeserializer;
+import net.sergeych.biserializer.BiSerializer;
+import net.sergeych.tools.Binder;
+import org.junit.Before;
+import org.junit.Test;
+import java.time.ZonedDateTime;
+import static org.junit.Assert.assertEquals;
+
+public class ParcelTest {
+    public Binder serialize(Parcel parcel) throws Exception {
+        BiSerializer biS = new BiSerializer();
+        return parcel.serialize(biS);
+    }
+
+    public Parcel deserialize(Binder binder) throws Exception {
+        BiDeserializer biD = new BiDeserializer();
+        return new Parcel(binder, biD);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void SerializeDeserialize() throws Exception {
+        TransactionPack payload_tp = new TransactionPack();
+        TransactionPack payment_tp = new TransactionPack();
+
+        //fill in with the values
+        Contract payload = new Contract();
+        payload.setIssuerKeys(TestKeys.publicKey(3));
+        payload.addSignerKey(TestKeys.privateKey(3));
+        payload.registerRole(new RoleLink("owner", "issuer"));
+        payload.registerRole(new RoleLink("creator", "issuer"));
+        payload.setExpiresAt(ZonedDateTime.now().plusDays(2));
+        payload.seal();
+
+        Contract payment = new Contract();
+        payment.setIssuerKeys(TestKeys.publicKey(3));
+        payment.addSignerKey(TestKeys.privateKey(3));
+        payment.registerRole(new RoleLink("owner", "issuer"));
+        payment.registerRole(new RoleLink("creator", "issuer"));
+        payment.setExpiresAt(ZonedDateTime.now().plusDays(2));
+        payment.seal();
+
+        payload_tp.setContract(payload);
+        payment_tp.setContract(payment);
+
+        //create parcel
+        Parcel parcel = new Parcel(payload_tp, payment_tp);
+
+        //serialize
+        Binder b = serialize(parcel);
+
+        //deserialize
+        Parcel des_parcel = deserialize(b);
+
+        //few assertions
+        assertEquals(parcel.getPayload().getContract().getId(), des_parcel.getPayload().getContract().getId());
+        assertEquals(parcel.getPayment().getContract().getId(), des_parcel.getPayment().getContract().getId());
+
+        assertEquals(parcel.getPayload().getContract().getState().getBranchId(), des_parcel.getPayload().getContract().getState().getBranchId());
+        assertEquals(parcel.getPayment().getContract().getState().getBranchId(), des_parcel.getPayment().getContract().getState().getBranchId());
+
+        assertEquals(parcel.getPayload().getContract().getState().getCreatedAt().getSecond(), des_parcel.getPayload().getContract().getState().getCreatedAt().getSecond());
+        assertEquals(parcel.getPayment().getContract().getState().getCreatedAt().getSecond(), des_parcel.getPayment().getContract().getState().getCreatedAt().getSecond());
+
+        assertEquals(parcel.getPayload().getContract().getExpiresAt().getDayOfYear(), des_parcel.getPayload().getContract().getExpiresAt().getDayOfYear());
+        assertEquals(parcel.getPayment().getContract().getExpiresAt().getDayOfYear(), des_parcel.getPayment().getContract().getExpiresAt().getDayOfYear());
+
+        assertEquals(parcel.getPayload().getReferences(), des_parcel.getPayload().getReferences());
+        assertEquals(parcel.getPayment().getReferences(), des_parcel.getPayment().getReferences());
+    }
+}
