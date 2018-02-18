e0a7e3bd934628a131f571d29bf9e88a697e8dac
Alex Myodov <amyodov@gmail.com>
2018-02-18 19:21:03 +0200

    Now NativeRSAEngine (GMP-optimized implementation) is used only if it is running perfectly.

--- crypto/src/main/java/com/icodici/crypto/rsaoaep/RSAOAEPEngine.java
+++ crypto/src/main/java/com/icodici/crypto/rsaoaep/RSAOAEPEngine.java
@@ -9,12 +9,39 @@ package com.icodici.crypto.rsaoaep;
 
 import com.icodici.crypto.rsaoaep.scrsa.NativeRSAEngine;
 import org.spongycastle.crypto.engines.RSAEngine;
+import org.spongycastle.crypto.params.ParametersWithRandom;
 
 /**
  * One-stop shop to create an new RSA engine (which implementation may vary).
  */
 public class RSAOAEPEngine {
+
+    protected final static boolean shouldUseNative = whetherShouldUseNative();
+
+    /**
+     * Create the {@link RSAEngine} implementation using the best possible optimization.
+     */
     public static RSAEngine make() {
-        return new NativeRSAEngine();
+        return (shouldUseNative) ? new NativeRSAEngine() : new RSAEngine();
+    }
+
+    /**
+     * Perform a test to check whether we should use an optimized native implementation or default Java one.
+     */
+    static boolean whetherShouldUseNative() {
+        // Sometimes NativeRSAEngine does not work; e.g. when GMP native binary is not available.
+        // Test once if it will work; if it doesn't, fallback to use default RSAEngine.
+        final RSAOAEPTestVectors oaepSpec = new RSAOAEPTestVectors();
+        final ParametersWithRandom param = new ParametersWithRandom(oaepSpec.pubParameters, oaepSpec.getRandSeed());
+
+        boolean errorOccured;
+        try {
+            final NativeRSAEngine nativeRSAEngine = new NativeRSAEngine();
+            nativeRSAEngine.init(true, param);
+            errorOccured = false;
+        } catch (Exception e) {
+            errorOccured = true;
+        }
+        return !errorOccured;
     }
 }
