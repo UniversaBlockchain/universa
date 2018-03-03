8205a940901cf5cc9cccee8694479db7b7587f2b
stipan <micromillioner@yahoo.com>
2018-03-03 02:22:23 -0800

    Added missing files

---
+++ universa_core/src/main/java/com/icodici/universa/contract/InnerContractsService.java
@@ -0,0 +1,77 @@
+package com.icodici.universa.contract;
+
+import com.icodici.crypto.PrivateKey;
+import com.icodici.crypto.PublicKey;
+import com.icodici.universa.contract.roles.SimpleRole;
+import com.icodici.universa.node2.Config;
+import net.sergeych.tools.Do;
+
+import java.io.IOException;
+import java.util.Set;
+
+/**
+ * Developer's methods for help with creating and preparing contracts.
+ */
+public class InnerContractsService {
+
+
+
+    /**
+     * Creates fresh contract in the first revision with transaction units.
+     *<br><br>
+     * This contract should be registered and then should be used as payment for other contract's processing.
+     * TU contracts signs with special Universa keys and set as owner public keys from params.
+     *<br><br>
+     * @param amount is initial number of TU that will be have an owner
+     * @param ownerKeys is public keys that will became an owner of TU     *
+     * @return sealed TU contract; should be registered in the Universa by simplified procedure.
+     * @throws IOException with exceptions while contract preparing
+     */
+    public synchronized static Contract createFreshTU(int amount, Set<PublicKey> ownerKeys) throws IOException {
+
+        return createFreshTU(amount, ownerKeys, false);
+    }
+
+
+    /**
+     * Creates fresh contract in the first revision with transaction units.
+     *<br><br>
+     * This contract should be registered and then should be used as payment for other contract's processing.
+     * TU contracts signs with special Universa keys and set as owner public keys from params.
+     *<br><br>
+     * @param amount is initial number of TU that will be have an owner
+     * @param ownerKeys is public keys that will became an owner of TU
+     * @param withTestTU if true TU will be created with test transaction units
+     * @return sealed TU contract; should be registered in the Universa by simplified procedure.
+     * @throws IOException with exceptions while contract preparing
+     */
+    public synchronized static Contract createFreshTU(int amount, Set<PublicKey> ownerKeys, boolean withTestTU) throws IOException {
+
+        PrivateKey manufacturePrivateKey = new PrivateKey(Do.read(Config.tuKeyPath));
+        Contract tu;
+        if(withTestTU) {
+            tu = Contract.fromDslFile(Config.testTUTemplatePath);
+        } else {
+            tu = Contract.fromDslFile(Config.tuTemplatePath);
+        }
+
+        SimpleRole ownerRole = new SimpleRole("owner");
+        for (PublicKey k : ownerKeys) {
+            KeyRecord kr = new KeyRecord(k);
+            ownerRole.addKeyRecord(kr);
+        }
+
+        tu.registerRole(ownerRole);
+        tu.createRole("owner", ownerRole);
+
+        tu.getStateData().set("transaction_units", amount);
+        if(withTestTU) {
+            tu.getStateData().set("test_transaction_units", amount * 100);
+        }
+
+        tu.addSignerKey(manufacturePrivateKey);
+        tu.seal();
+
+        return tu;
+    }
+}
