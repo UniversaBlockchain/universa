35c2bc7187adeb329a3795d030fe4230a2943507
stipan <micromillioner@yahoo.com>
2018-04-17 08:36:28 -0700

    Added creating SmartContract from dsl

--- universa_core/src/main/java/com/icodici/universa/contract/SmartContract.java
+++ universa_core/src/main/java/com/icodici/universa/contract/SmartContract.java
@@ -22,7 +22,6 @@ import java.util.Map;
 import static com.icodici.universa.Errors.BAD_VALUE;
 import static com.icodici.universa.Errors.FAILED_CHECK;
 
-@BiType(name = "UniversaSmartContract")
 public class SmartContract extends Contract implements NodeContract {
 
 
