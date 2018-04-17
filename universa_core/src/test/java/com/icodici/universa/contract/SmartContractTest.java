469cc61dc6f8d72ea3218dc0ae6c0e09b5784415
stipan <micromillioner@yahoo.com>
2018-04-17 08:36:28 -0700

    Added creating SmartContract from dsl

--- universa_core/src/test/java/com/icodici/universa/contract/SmartContractTest.java
+++ universa_core/src/test/java/com/icodici/universa/contract/SmartContractTest.java
@@ -20,6 +20,28 @@ public class SmartContractTest extends ContractTestBase {
         smartContract.check();
         smartContract.traceErrors();
         assertTrue(smartContract.isOk());
+
+        assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), smartContract.getDefinition().getExtendedType());
+        assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), smartContract.get("definition.extended_type"));
+
+        assertTrue(smartContract instanceof SmartContract);
+        assertTrue(smartContract instanceof NodeSmartContract);
+    }
+
+    @Test
+    public void goodSmartContractFromDSL() throws Exception {
+        Contract smartContract = SmartContract.fromDslFile(rootPath + "NotarySmartDSLTemplate.yml");
+        smartContract.addSignerKeyFromFile(rootPath + "_xer0yfe2nn1xthc.private.unikey");
+        smartContract.seal();
+        smartContract.check();
+        smartContract.traceErrors();
+        assertTrue(smartContract.isOk());
+
+        assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), smartContract.getDefinition().getExtendedType());
+        assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), smartContract.get("definition.extended_type"));
+
+        assertTrue(smartContract instanceof SmartContract);
+        assertTrue(smartContract instanceof NodeSmartContract);
     }
 
     @Test
@@ -36,10 +58,14 @@ public class SmartContractTest extends ContractTestBase {
         assertSameContracts(smartContract, desContract);
         assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), desContract.getDefinition().getExtendedType());
         assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), desContract.get("definition.extended_type"));
+        assertTrue(desContract instanceof SmartContract);
+        assertTrue(desContract instanceof NodeSmartContract);
 
         Contract copiedContract = smartContract.copy();
         assertSameContracts(smartContract, copiedContract);
         assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), copiedContract.getDefinition().getExtendedType());
         assertEquals(SmartContract.SmartContractType.DEFAULT_SMART_CONTRACT.name(), copiedContract.get("definition.extended_type"));
+        assertTrue(copiedContract instanceof SmartContract);
+        assertTrue(copiedContract instanceof NodeSmartContract);
     }
 }
