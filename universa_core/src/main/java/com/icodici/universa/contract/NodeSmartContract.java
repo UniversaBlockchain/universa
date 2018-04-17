36fb6ba1920eb6478a7c53afd9d34ec2bb5cc5b9
sergeych <real.sergeych@gmail.com>
2018-04-17 08:19:25 +0200

    nodesnartcontract interface extended

--- universa_core/src/main/java/com/icodici/universa/contract/NodeSmartContract.java
+++ universa_core/src/main/java/com/icodici/universa/contract/NodeSmartContract.java
@@ -1,10 +1,13 @@
 package com.icodici.universa.contract;
 
+import net.sergeych.tools.Binder;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 /**
  * Node-side smart contract handler.
- *
- * Used to implement node-side smart contract functionality (e.g. slot contract and other incoming
- * types)
+ * <p>
+ * Used to implement node-side smart contract functionality (e.g. slot contract and other incoming types)
  */
 public interface NodeSmartContract {
 
@@ -14,7 +17,7 @@ public interface NodeSmartContract {
      *
      * @return string tag, e.g. "SLOT1"
      */
-    String getExtendedType();
+    @NonNull String getExtendedType();
 
     /**
      * Check the smartcontract could be created
@@ -53,20 +56,35 @@ public interface NodeSmartContract {
      *
      * @param c
      *         contract just approved
+     *
+     * @return extra data to pass to the calling client or null
      */
-    void onCreated(Contract c);
+    @Nullable Binder onCreated(Contract c);
 
     /**
      * Called after the new contract revision is approved by the network.
      *
      * @param c
      *         contract
+     *
+     * @return extra data to pass to the calling client or null
      */
-    void onUpdate(Contract c);
+    @Nullable Binder onUpdate(Contract c);
 
     /**
      * Called when the contract is just revoked by the network
+     *
      * @param c
      */
     void onRevoke(Contract c);
+
+    /**
+     * Call the readonly method (query) that does not change the contract inner state (neither the contract nor
+     * any associated data) and return the result
+     *
+     * @param methodName
+     * @param params or null
+     * @return the results
+     */
+    @NonNull Binder query(String methodName, Binder params);
 }
