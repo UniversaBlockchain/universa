3efdd296cb3fda7997beac4e4539022895db9f32
stipan <micromillioner@yahoo.com>
2018-04-17 07:39:11 -0700

    Added extra result to ItemResult after registering and check item.
    Added missed files

--- universa_core/src/main/java/com/icodici/universa/contract/SmartContract.java
+++ universa_core/src/main/java/com/icodici/universa/contract/SmartContract.java
@@ -12,7 +12,6 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Map;
