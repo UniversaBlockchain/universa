338a3c5600d17f61eab7e4a50e45c508466b3ed8
unknown <kiril@solinter.net>
2018-05-21 23:25:23 +0200

    Docker update, 4 nodes

---
+++ docker/init-node.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+PRIVATE_DIR="/deploy/tmp"
+
+echo "test"
+
+if ! [ "$(ls -A $PRIVATE_DIR)" ]; then
+	java -jar /code/uniclient/build/libs/uniclient.jar -g node-${NODE_INDEX}-local.universa.io
+	mv ./node-${NODE_INDEX}-local.universa.io.public.unikey /deploy/config/keys/node-${NODE_INDEX}-local.universa.io.public.unikey
+	mv ./node-${NODE_INDEX}-local.universa.io.private.unikey /deploy/tmp/node-${NODE_INDEX}-local.universa.io.private.unikey
+fi
+
+java -jar /code/universa_core/build/output/uninode.jar -c .
---
+++ docker/init-user-db.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER node1user;
+	ALTER USER node1user with encrypted password 'uniPass';
+	CREATE DATABASE node1db;
+	GRANT ALL PRIVILEGES ON DATABASE node1db TO node1user;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER node2user;
+	ALTER USER node2user with encrypted password 'uniPass';
+	CREATE DATABASE node2db;
+	GRANT ALL PRIVILEGES ON DATABASE node2db TO node2user;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER node3user;
+	ALTER USER node3user with encrypted password 'uniPass';
+	CREATE DATABASE node3db;
+	GRANT ALL PRIVILEGES ON DATABASE node3db TO node3user;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER node4user;
+	ALTER USER node4user with encrypted password 'uniPass';
+	CREATE DATABASE node4db;
+	GRANT ALL PRIVILEGES ON DATABASE node4db TO node4user;
+EOSQL
