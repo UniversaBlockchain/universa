a3d5e748bf7c2a309e0dac1c6fad80b1395a722c
unknown <kiril@solinter.net>
2018-06-03 16:26:31 +0200

    Dockerfile optimization

--- docker/init-node.sh
+++ docker/init-node.sh
@@ -3,9 +3,9 @@ set -e
 PRIVATE_DIR="/deploy/tmp"
 
 if ! [ "$(ls -A $PRIVATE_DIR)" ]; then
-	java -jar /code/uniclient/build/libs/uniclient.jar -g node-${NODE_INDEX}-local.universa.io
+	java -jar /deploy/build-client/libs/uniclient.jar -g node-${NODE_INDEX}-local.universa.io
 	mv ./node-${NODE_INDEX}-local.universa.io.public.unikey /deploy/config/keys/node-${NODE_INDEX}-local.universa.io.public.unikey
 	mv ./node-${NODE_INDEX}-local.universa.io.private.unikey /deploy/tmp/node-${NODE_INDEX}-local.universa.io.private.unikey
 fi
 
-java -jar /code/universa_core/build/output/uninode.jar -c /deploy
+java -jar /deploy/build-core/output/uninode.jar -c /deploy
