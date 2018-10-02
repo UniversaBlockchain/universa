3b97c55712ac84fc0603ed3cb1b8da51b6e34458
kalinkineo <kalinkineo@gmail.com>
2018-10-03 02:33:45 +0300

    added FollowerDSLTemplate.yml and updated FollowerContractTest

--- universa_core/src/test/java/com/icodici/universa/contract/FollowerContractTest.java
+++ universa_core/src/test/java/com/icodici/universa/contract/FollowerContractTest.java
@@ -5,6 +5,7 @@ import com.icodici.crypto.PrivateKey;
 import com.icodici.crypto.PublicKey;
 import com.icodici.universa.contract.permissions.ModifyDataPermission;
 import com.icodici.universa.contract.permissions.Permission;
+import com.icodici.universa.contract.services.FollowerContract;
 import com.icodici.universa.contract.services.NSmartContract;
 import com.icodici.universa.contract.services.SlotContract;
 import com.icodici.universa.node2.Config;
@@ -39,89 +40,89 @@ public class FollowerContractTest extends ContractTestBase {
     public void goodFollowerContract() throws Exception {
 
         final PrivateKey key = new PrivateKey(Do.read(rootPath + "_xer0yfe2nn1xthc.private.unikey"));
+        final PrivateKey key2 = new PrivateKey(Do.read(rootPath + "test_network_whitekey.private.unikey"));
 
-        Contract simpleContract = new Contract(key);
+        Contract simpleContract = new Contract(key2);
         simpleContract.seal();
+        simpleContract.check();
+        simpleContract.traceErrors();
+        assertTrue(simpleContract.isOk());
 
-        Contract paymentDecreased = createSlotPayment();
+        PrivateKey privateKey = new PrivateKey(2048);
+        PublicKey callbackKey = privateKey.getPublicKey();
 
-        Contract smartContract = new SlotContract(key);
+        Contract smartContract = new FollowerContract(key);
 
-        assertTrue(smartContract instanceof SlotContract);
+        assertTrue(smartContract instanceof FollowerContract);
 
-        ((SlotContract)smartContract).putTrackingContract(simpleContract);
-        ((SlotContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
-        smartContract.addNewItems(paymentDecreased);
+        ((FollowerContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
+        ((FollowerContract)smartContract).putTrackingOrigin(simpleContract.getOrigin(), "http:\\\\localhost:7777\\follow.callback", callbackKey);
+
         smartContract.seal();
         smartContract.check();
         smartContract.traceErrors();
         assertTrue(smartContract.isOk());
 
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), smartContract.getDefinition().getExtendedType());
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), smartContract.get("definition.extended_type"));
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), smartContract.getDefinition().getExtendedType());
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), smartContract.get("definition.extended_type"));
 
         Multimap<String, Permission> permissions = smartContract.getPermissions();
         Collection<Permission> mdp = permissions.get("modify_data");
         assertNotNull(mdp);
         assertTrue(((ModifyDataPermission)mdp.iterator().next()).getFields().containsKey("action"));
 
-        assertEquals(simpleContract.getId(), ((SlotContract) smartContract).getTrackingContract().getId());
-        assertEquals(simpleContract.getId(), TransactionPack.unpack(((SlotContract) smartContract).getPackedTrackingContract()).getContract().getId());
+        assertEquals(((FollowerContract) smartContract).getCallbackKeys().get("http:\\\\localhost:7777\\follow.callback"),callbackKey );
+        assertEquals(((FollowerContract) smartContract).getTrackingOrigins().get(simpleContract.getOrigin()),
+                "http:\\\\localhost:7777\\follow.callback");
+        assertTrue(((FollowerContract) smartContract).isOriginTracking(simpleContract.getOrigin()));
+        assertTrue(((FollowerContract) smartContract).isCallbackURLUsed("http:\\\\localhost:7777\\follow.callback"));
 
-        Binder trackingHashesAsBase64 = smartContract.getStateData().getBinder("tracking_contract");
-        for (String k : trackingHashesAsBase64.keySet()) {
-            byte[] packed = trackingHashesAsBase64.getBinary(k);
-            if (packed != null) {
-                Contract c = Contract.fromPackedTransaction(packed);
-                assertEquals(simpleContract.getId(), c.getId());
-            }
-        }
     }
 
     @Test
     public void goodSmartContractFromDSL() throws Exception {
 
-        final PrivateKey key = new PrivateKey(Do.read(rootPath + "_xer0yfe2nn1xthc.private.unikey"));
-        Contract simpleContract = new Contract(key);
+        final PrivateKey key2 = new PrivateKey(Do.read(rootPath + "test_network_whitekey.private.unikey"));
+
+        Contract simpleContract = new Contract(key2);
         simpleContract.seal();
+        simpleContract.check();
+        simpleContract.traceErrors();
+        assertTrue(simpleContract.isOk());
 
-        Contract paymentDecreased = createSlotPayment();
+        PrivateKey privateKey = new PrivateKey(2048);
+        PublicKey callbackKey = privateKey.getPublicKey();
 
-        Contract smartContract = SlotContract.fromDslFile(rootPath + "SlotDSLTemplate.yml");
+        //Contract smartContract = new FollowerContract(key);
+        Contract smartContract = FollowerContract.fromDslFile(rootPath + "FollowerDSLTemplate.yml");
         smartContract.addSignerKeyFromFile(rootPath + "_xer0yfe2nn1xthc.private.unikey");
 
-        assertTrue(smartContract instanceof SlotContract);
+        assertTrue(smartContract instanceof FollowerContract);
 
-        ((SlotContract)smartContract).putTrackingContract(simpleContract);
-        ((SlotContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
-        smartContract.addNewItems(paymentDecreased);
+        ((FollowerContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
+        ((FollowerContract)smartContract).putTrackingOrigin(simpleContract.getOrigin(), "http:\\\\localhost:7777\\follow.callback", callbackKey);
+
         smartContract.seal();
         smartContract.check();
         smartContract.traceErrors();
         assertTrue(smartContract.isOk());
 
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), smartContract.getDefinition().getExtendedType());
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), smartContract.get("definition.extended_type"));
-
-        assertEquals(2, ((SlotContract) smartContract).getKeepRevisions());
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), smartContract.getDefinition().getExtendedType());
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), smartContract.get("definition.extended_type"));
 
         Multimap<String, Permission> permissions = smartContract.getPermissions();
         Collection<Permission> mdp = permissions.get("modify_data");
         assertNotNull(mdp);
         assertTrue(((ModifyDataPermission)mdp.iterator().next()).getFields().containsKey("action"));
 
-        assertEquals(simpleContract.getId(), ((SlotContract) smartContract).getTrackingContract().getId());
-        assertEquals(simpleContract.getId(), TransactionPack.unpack(((SlotContract) smartContract).getPackedTrackingContract()).getContract().getId());
+        assertEquals(((FollowerContract) smartContract).getCallbackKeys().get("http:\\\\localhost:7777\\follow.callback"),callbackKey );
+        assertEquals(((FollowerContract) smartContract).getTrackingOrigins().get(simpleContract.getOrigin()),
+                "http:\\\\localhost:7777\\follow.callback");
+        assertTrue(((FollowerContract) smartContract).isOriginTracking(simpleContract.getOrigin()));
+        assertTrue(((FollowerContract) smartContract).isCallbackURLUsed("http:\\\\localhost:7777\\follow.callback"));
 
-        Binder trackingHashesAsBase64 = smartContract.getStateData().getBinder("tracking_contract");
-        for (String k : trackingHashesAsBase64.keySet()) {
-            byte[] packed = trackingHashesAsBase64.getBinary(k);
-            if (packed != null) {
-                Contract c = Contract.fromPackedTransaction(packed);
-                assertEquals(simpleContract.getId(), c.getId());
-            }
-        }
     }
+
     private NSmartContract.NodeInfoProvider nodeInfoProvider = new NSmartContract.NodeInfoProvider() {
 
         Config config = new Config();
@@ -158,69 +159,70 @@ public class FollowerContractTest extends ContractTestBase {
     @Test
     public void serializeSmartContract() throws Exception {
         final PrivateKey key = new PrivateKey(Do.read(rootPath + "_xer0yfe2nn1xthc.private.unikey"));
-        Contract simpleContract = new Contract(key);
+        final PrivateKey key2 = new PrivateKey(Do.read(rootPath + "test_network_whitekey.private.unikey"));
+
+        Contract simpleContract = new Contract(key2);
         simpleContract.seal();
+        simpleContract.check();
+        simpleContract.traceErrors();
+        assertTrue(simpleContract.isOk());
 
-        Contract paymentDecreased = createSlotPayment();
+        PrivateKey privateKey = new PrivateKey(2048);
+        PublicKey callbackKey = privateKey.getPublicKey();
 
-        Contract smartContract = new SlotContract(key);
+        Contract smartContract = new FollowerContract(key);
 
-        assertTrue(smartContract instanceof SlotContract);
+        assertTrue(smartContract instanceof FollowerContract);
 
-        ((SlotContract)smartContract).putTrackingContract(simpleContract);
-        ((SlotContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
-        smartContract.addNewItems(paymentDecreased);
+        ((FollowerContract)smartContract).setNodeInfoProvider(nodeInfoProvider);
+
         smartContract.seal();
         smartContract.check();
         smartContract.traceErrors();
         assertTrue(smartContract.isOk());
 
         Binder b = BossBiMapper.serialize(smartContract);
+
         Contract desContract = DefaultBiMapper.deserialize(b);
         assertSameContracts(smartContract, desContract);
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), desContract.getDefinition().getExtendedType());
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), desContract.get("definition.extended_type"));
-        assertTrue(desContract instanceof SlotContract);
+
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), desContract.getDefinition().getExtendedType());
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), desContract.get("definition.extended_type"));
 
         Multimap<String, Permission> permissions = desContract.getPermissions();
         Collection<Permission> mdp = permissions.get("modify_data");
         assertNotNull(mdp);
         assertTrue(((ModifyDataPermission)mdp.iterator().next()).getFields().containsKey("action"));
 
-        assertEquals(simpleContract.getId(), ((SlotContract) desContract).getTrackingContract().getId());
-        assertEquals(simpleContract.getId(), TransactionPack.unpack(((SlotContract) desContract).getPackedTrackingContract()).getContract().getId());
+        ((FollowerContract)desContract).putTrackingOrigin(simpleContract.getOrigin(), "http:\\\\localhost:7777\\follow.callback", callbackKey);
 
-        Binder trackingHashesAsBase64 = desContract.getStateData().getBinder("tracking_contract");
-        for (String k : trackingHashesAsBase64.keySet()) {
-            byte[] packed = trackingHashesAsBase64.getBinary(k);
-            if (packed != null) {
-                Contract c = Contract.fromPackedTransaction(packed);
-                assertEquals(simpleContract.getId(), c.getId());
-            }
-        }
+        assertEquals(((FollowerContract) desContract).getCallbackKeys().get("http:\\\\localhost:7777\\follow.callback"),callbackKey );
+        assertEquals(((FollowerContract) desContract).getTrackingOrigins().get(simpleContract.getOrigin()),
+                "http:\\\\localhost:7777\\follow.callback");
+        assertTrue(((FollowerContract) desContract).isOriginTracking(simpleContract.getOrigin()));
+        assertTrue(((FollowerContract) desContract).isCallbackURLUsed("http:\\\\localhost:7777\\follow.callback"));
 
         Contract copiedContract = smartContract.copy();
         assertSameContracts(smartContract, copiedContract);
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), copiedContract.getDefinition().getExtendedType());
-        assertEquals(NSmartContract.SmartContractType.SLOT1.name(), copiedContract.get("definition.extended_type"));
-        assertTrue(copiedContract instanceof SlotContract);
 
-        permissions = copiedContract.getPermissions();
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), copiedContract.getDefinition().getExtendedType());
+        assertEquals(NSmartContract.SmartContractType.FOLLOWER1.name(), copiedContract.get("definition.extended_type"));
+
+        assertTrue(copiedContract instanceof FollowerContract);
+
+        permissions = desContract.getPermissions();
         mdp = permissions.get("modify_data");
         assertNotNull(mdp);
         assertTrue(((ModifyDataPermission)mdp.iterator().next()).getFields().containsKey("action"));
 
-        assertEquals(simpleContract.getId(), ((SlotContract) copiedContract).getTrackingContract().getId());
-        assertEquals(simpleContract.getId(), TransactionPack.unpack(((SlotContract) copiedContract).getPackedTrackingContract()).getContract().getId());
+        ((FollowerContract)copiedContract).putTrackingOrigin(simpleContract.getOrigin(), "http:\\\\localhost:7777\\follow.callback", callbackKey);
 
-        trackingHashesAsBase64 = copiedContract.getStateData().getBinder("tracking_contract");
-        for (String k : trackingHashesAsBase64.keySet()) {
-            byte[] packed = trackingHashesAsBase64.getBinary(k);
-            if (packed != null) {
-                Contract c = Contract.fromPackedTransaction(packed);
-                assertEquals(simpleContract.getId(), c.getId());
-            }
-        }
+        assertEquals(((FollowerContract) copiedContract).getCallbackKeys().get("http:\\\\localhost:7777\\follow.callback"),callbackKey );
+        assertEquals(((FollowerContract) copiedContract).getTrackingOrigins().get(simpleContract.getOrigin()),
+                "http:\\\\localhost:7777\\follow.callback");
+        assertTrue(((FollowerContract) copiedContract).isOriginTracking(simpleContract.getOrigin()));
+        assertTrue(((FollowerContract) copiedContract).isCallbackURLUsed("http:\\\\localhost:7777\\follow.callback"));
+
    }
 
     @Test
