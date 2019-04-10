4f64c74d6744539bdaa0f435544edd59d098dabf
Roman Uskov <roman.uskov@gmail.com>
2019-04-10 15:03:13 +0100

    v3.9.7 Client/Uniclient tool can now use topology from file or cached topology by name

--- universa_core/src/main/java/com/icodici/universa/Core.java
+++ universa_core/src/main/java/com/icodici/universa/Core.java
@@ -1,7 +1,7 @@
 package com.icodici.universa;
 
 public class Core {
-    static public final String VERSION = "3.9.6";
+    static public final String VERSION = "3.9.7";
     static public String getVersion() {
         return VERSION;
     }
