9cc3623c34f4daa49ef51e2bac1a1c96015ccd37
Roman Uskov <roman.uskov@gmail.com>
2019-04-21 22:01:06 +0300

    v3.9.9 topology construction optimization

--- universa_core/src/main/java/com/icodici/universa/Core.java
+++ universa_core/src/main/java/com/icodici/universa/Core.java
@@ -1,7 +1,7 @@
 package com.icodici.universa;
 
 public class Core {
-    static public final String VERSION = "3.9.8";
+    static public final String VERSION = "3.9.9";
     static public String getVersion() {
         return VERSION;
     }
