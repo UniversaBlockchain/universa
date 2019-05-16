d2485c7da771824b40be8c98603fdb0786e16987
Roman Uskov <roman.uskov@gmail.com>
2019-05-16 13:46:13 +0300

    v3.9.12 node proxy 'onion' requests protection, UPD moved to ipv6

--- universa_core/src/main/java/com/icodici/universa/Core.java
+++ universa_core/src/main/java/com/icodici/universa/Core.java
@@ -1,7 +1,7 @@
 package com.icodici.universa;
 
 public class Core {
-    static public final String VERSION = "3.9.11";
+    static public final String VERSION = "3.9.12";
     static public String getVersion() {
         return VERSION;
     }
