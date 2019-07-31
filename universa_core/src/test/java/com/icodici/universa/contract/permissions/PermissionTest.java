805b4dd1bc53898ab1f954d586335589665c537a
Roman Uskov <roman.uskov@gmail.com>
2019-07-31 17:24:45 +0300

    permission test: equality/using in references

--- universa_core/src/test/java/com/icodici/universa/contract/permissions/PermissionTest.java
+++ universa_core/src/test/java/com/icodici/universa/contract/permissions/PermissionTest.java
@@ -2,8 +2,10 @@ package com.icodici.universa.contract.permissions;
 
 import com.icodici.universa.TestKeys;
 import com.icodici.universa.contract.Contract;
+import com.icodici.universa.contract.Reference;
 import com.icodici.universa.contract.roles.RoleLink;
 import net.sergeych.tools.Binder;
+import net.sergeych.tools.Do;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -82,6 +84,47 @@ public class PermissionTest {
 
         assertNotNull(contract.get("definition.permissions.changenumber"));
         assertEquals((Permission)contract.get("definition.permissions.changenumber"),contract2.get("definition.permissions.changenumber"));
+    }
+
+
+    @Test
+    public void contractRefToPermission() throws Exception {
+        Contract contract = new Contract(TestKeys.privateKey(0));
+        Contract contract2 = new Contract(TestKeys.privateKey(0));
+        ChangeNumberPermission changeNumberPermission = new ChangeNumberPermission(new RoleLink("@owner", "owner"), Binder.of(
+                "field_name", "field1",
+                "min_value", 33,
+                "max_value", 34,
+                "min_step", 1,
+                "max_step", 1
+        )
+        );
+
+        ChangeNumberPermission changeNumberPermission2 = new ChangeNumberPermission(new RoleLink("@owner", "owner"), Binder.of(
+                "field_name", "field1",
+                "min_value", 33,
+                "max_value", 34,
+                "min_step", 1,
+                "max_step", 1
+        )
+        );
+        changeNumberPermission.setId("changenumber");
+        changeNumberPermission2.setId("changenumber2");
+
+        contract.addPermission(changeNumberPermission);
+        contract2.addPermission(changeNumberPermission2);
+        contract.seal();
+
+
+        Reference reference = new Reference(contract2);
+        reference.name = "test1";
+        reference.setConditions(Binder.of("all_of", Do.listOf("ref.id==\""+contract.getId().toBase64String()+"\"","this.definition.permissions.changenumber2==ref.definition.permissions.changenumber")));
+        contract2.addReference(reference);
+
+        contract2.seal();
+        contract2.getTransactionPack().addReferencedItem(contract);
+        contract2 = Contract.fromPackedTransaction(contract2.getPackedTransaction());
+        assertTrue(contract2.check());
     }
 
 }
