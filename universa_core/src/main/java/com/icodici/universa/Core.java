79c1eb68ea8ec3c430a7f29b51b40d58e8f0aeeb
Roman Uskov <roman.uskov@gmail.com>
2019-08-01 16:01:12 +0300

    v3.10.2 - Reference type (section) is not ignored with comparing two references for equality in conditions.

--- universa_core/src/main/java/com/icodici/universa/Core.java
+++ universa_core/src/main/java/com/icodici/universa/Core.java
@@ -1,7 +1,7 @@
 package com.icodici.universa;
 
 public class Core {
-    static public final String VERSION = "3.10.1";
+    static public final String VERSION = "3.10.2";
     static public String getVersion() {
         return VERSION;
     }
